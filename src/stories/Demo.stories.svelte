abe5f30d7b7507477cc0e500b17153eb8a3b02be
Konstantin Bläsi <blaesi@amiconsult.de>
2022-11-14 09:28:05 +0100

    TypeScript component and Story using Svelte format

---
+++ src/stories/Demo.stories.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {Meta, Template, Story} from "@storybook/addon-svelte-csf"
+    import Demo from "$lib/Demo.svelte";
+
+    let defaultArgs = {
+        value: "Hello World"
+    }
+    function withDefaults(args) {
+        return {
+            ...defaultArgs,
+            ...args
+        }
+    }
+</script>
+
+<Meta title="Example/Demo" component={Demo}/>
+
+<Template let:args>
+    <Demo {...withDefaults(args)} />
+</Template>
+
+<Story name="Default" args={{}}/>
